f168291b067ca5cd5ab4bb44f2a067fc95ea66d0
Hannah Simonelli <s538903@nwmissouri.edu>
2024-07-05 15:47:50 -0400

    https://hgsimonelli.github.io/df-mod2-forensic-copy/

--- Evidence Hashes/Evidence Hashes Collection.ps1
+++ Evidence Hashes/Evidence Hashes Collection.ps1
@@ -4,3 +4,5 @@ Get-FileHash -Path evidence\Gremlin.jpg  -Algorithm SHA256
 Get-FileHash -Path evidence\Textfileevidence.txt  -Algorithm SHA256
 Get-FileHash -Path evidence\VM.E01 -Algorithm SHA256
 Get-FileHash -Path .\evidence\Weddingphoto.jpg  -Algorithm SHA256
+
+# Hannah Simonelli
